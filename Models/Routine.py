d8195359163dc036ffaa09f8e58ba0590a04c49e
BuildTools <unconfigured@null.spigotmc.org>
2017-12-01 17:39:59 +0000

    missing files

---
+++ Models/Routine.py
@@ -0,0 +1,17 @@
+from Util.adb import ADB_Input
+import numbers
+import time
+
+class Routine(object):
+	"""docstring for Routine"""
+	def __init__(self):
+		return
+
+	def tap(self,steps):
+		for step in steps:
+			if isinstance(step, list):
+				if len(step) is 2:
+					ADB_Input.tap(step[0],step[1])
+
+			elif isinstance(step, numbers.Number):
+				time.sleep(step)
